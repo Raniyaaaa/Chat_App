74cd25966065e844ba88dc84943e46f1bfb34471
Raniyaaaa <raniya182002@gmail.com>
2025-03-01 12:09:54 +0530

    Creating Login Screen Frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Signup from "./components/Auth/Signup";
+import Login from "./components/Auth/Login";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       {/* <Navbar /> */}
       <Routes>
         <Route path="/signup" element={<Signup/>} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
---
+++ client/src/components/Auth/Login.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const navigate = useNavigate();
+
+  const handleChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_API_BASE_URL}/auth/login`,
+        formData
+      );
+      console.log(response.data);
+      
+      localStorage.setItem("token", response.data.token);
+      localStorage.setItem("userId", response.data.user.id);
+      navigate("/dashboard");
+    } catch (error) {
+      alert("Login failed");
+    }
+  };
+
+  return (
+    <div className="container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+          required
+        />
+        <button type="submit">Login</button>
+
+      </form>
+      <p className="form-text">
+        <a href="/forgot-password">Forgot Password?</a>
+      </p>
+      <p className="form-text">
+        Don't have an account? <a href="/signup">Signup</a>
+      </p>
+    </div>
+  );
+};
+
+export default Login;
